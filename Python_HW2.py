deb469d44e7b9f8e28ba872bf17bb623f27d51ef
Maximus2806 <morozof_78@mail.ru>
2022-02-15 10:38:46 +0300

    hw2 for checking

---
+++ Python_HW2.py
@@ -0,0 +1,70 @@
+#Arithmetic
+item_1 = 4
+item_2 = 10
+result_sum = item_1 + item_2
+print('Result_sum =', result_sum)
+result_subtr = item_2 - item_1
+print("result_subtr =", result_subtr)
+result_multi = item_1 * item_2
+print('result_multi =', result_multi)
+result_exp = item_1 ** item_2
+print('result_exp =', result_exp)
+import math
+result_m_exp = math.pow(item_1, item_2)
+print('result_m_exp =', result_m_exp)
+result_s_root = item_1 ** 0.5
+print('result_s_root =', result_s_root)
+result_m_s_root = math.sqrt(item_2)
+print('result_m_s_root =', result_m_s_root)
+result_mp_s_root = math.pow(item_1, 0.5)
+print('result_mp_s_root =', result_mp_s_root)
+item_1 = 15
+item_2 = 6
+result_division = item_1 / item_2
+print('result_division =', result_division)
+result_m_floor = math.floor(result_division)
+print('result_m_floor =', result_m_floor)
+result_m_ceil = math.ceil(result_division)
+print('result_m_ceil =', result_m_ceil)
+result_int = round(result_division)
+print('result_int =', result_int)
+result_no_division_loss = item_1 // item_2
+print('result_no_division_loss =', result_no_division_loss)
+result_division_loss = item_1 % item_2
+print('result_division_loss =', result_division_loss)
+#Дальше будет реализация через арифметические действия с присваиванием.
+item_3 = 5
+item_3 += 10
+print(item_3)
+item_3 -= 5
+print(item_3)
+item_3 *= 3
+print(item_3)
+item_3 /= 2
+print(item_3)
+item_3 **= 2
+print(item_3)
+item_3 **= 0.5
+print(item_3)
+item_3 %= 2
+print(item_3)
+print('-------------------')
+#Boolean
+b_item_t = True
+b_item_f = False
+b_item_result_sum = b_item_t + b_item_f
+print(b_item_result_sum)
+b_item_result_subtr = b_item_t - b_item_f
+print(b_item_result_subtr)
+b_item_result_multi = b_item_t * b_item_f
+print(b_item_result_multi)
+#b_item_result_division = b_item_t / b_item_f
+#print(b_item_result_division)
+b_item_1_int = int(b_item_t)
+print('b_item_1 =', b_item_1_int)
+b_item_2_int = int(b_item_f)
+print('b_item_2 =', b_item_2_int)
+print('HW2 is done!!!')
+
+
+
