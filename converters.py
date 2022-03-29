3587e65b21fb598d23b97fef555cc6bb39a222bb
Maximus2806 <morozof_78@mail.ru>
2022-03-29 13:46:16 +0300

    Add converters.py

---
+++ converters.py
@@ -0,0 +1,23 @@
+def dec_to_bin(num):
+    num_bin = []
+    while num != 0:
+        rest = num % 2
+        if rest == 0:
+            num_bin.append(0)
+        else:
+            num_bin.append(1)
+        num = num // 2
+    num_bin.reverse()
+    res_bin = ''.join(str(e) for e in num_bin)
+    return res_bin
+
+
+def bin_to_dec(str_bin):
+    m_power = len(str_bin) - 1
+    res_dec = 0
+    for i in str_bin:
+        res_dec += (int(i) * 2) ** m_power
+        m_power -= 1
+    return res_dec
+
+
