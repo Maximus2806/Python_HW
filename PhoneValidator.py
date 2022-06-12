8efbef537f4a8921fc5015ec57c6d600debabc82
Maximus2806 <morozof_78@mail.ru>
2022-06-12 22:19:39 +0300

    Add PhoneValidator.py

---
+++ PhoneValidator.py
@@ -0,0 +1,51 @@
+# Phone number validation
+# 1. Only digits are allowed
+# 2. Starts with 1,8,9 only
+# 3. May contain only 8 digits
+import re
+import time
+
+
+def check_first_dig(number):
+    pattern = r"^[1,8,9]"
+    if re.search(pattern, number):
+        return True
+    else:
+        global result
+        result = result + "The first digit must be 1,8 or 9 only. Try again."
+        return False
+
+
+def check_length(number):
+    if len(number) == 8:
+        return True
+    else:
+        global result
+        result = result + "Exactly 8 digits is needed. Try again"
+        return False
+
+
+def is_digit(number):
+    if number.isdigit():
+        return True
+    else:
+        global result
+        result = result + "Only digits are acceptable. Try again"
+        return False
+
+
+def validator(number):
+    if not check_length(num) or not check_first_dig(num) or not is_digit(num):
+        print(result)
+        time.sleep(1)
+    else:
+        global progress
+        progress = False
+        print("Accepted!")
+
+
+progress = True
+while progress:
+    result = "There is a problem: "
+    num = input("Input phone number: ")
+    validator(num)
